16e65614e9c30ff5a8addeddfe1c9fa3c6f9efcc
Mickael Urrutia <mickael.urrutia@utc.fr>
2022-01-27 14:11:13 +0100

    jour 08 terminé

--- day-08/afternoon.py
+++ day-08/afternoon.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 data_file = Path(__file__).absolute().parent / 'data.txt'
-data_file = Path(__file__).absolute().parent / 'data-example.txt'
+# data_file = Path(__file__).absolute().parent / 'data-example.txt'
 
 easy_nums = { 
     2: 1,
@@ -19,7 +19,7 @@ with open(data_file, 'r') as fd:
         parts = line.split('|')
         segments = [''.join(sorted(x)) for x in parts[0].strip().split(' ')]
         segments = sorted(segments, key=lambda x: len(x))
-        print(segments)
+
         digits = [''.join(sorted(x)) for x in parts[1].strip().split(' ')]
         
         inputs.append({ "segments": segments, "digits": digits})
@@ -31,7 +31,7 @@ def get_wiring(segments):
     cf = segments[0]
 
     # segment manquant entre 1 et 7
-    a = [x for x in cf if x not in segments[1]]
+    a = ''.join([x for x in cf if x not in segments[1]])
 
     # segments de 4 manquant dans 1
     bd = [x for x in segments[2] if x not in segments[0]]
@@ -43,40 +43,45 @@ def get_wiring(segments):
     cde = [x for x in segments[9] if x not in segments[6] or x not in segments[7] or x not in segments[8]]
 
     # croisement de cf / cde pour trouver c et déduire f
-    c = [x for x in cf if x in cde]
-    f = cf.replace(c, '')
+    c = ''.join([x for x in cf if x in cde])
+    f = ''.join([x for x in cf if x != c])
 
     # croisement eg / cde pour trouver e et déduire g
-    e = [x for x in eg if x in cde]
-    g = eg.replace(e, '')
+    e = ''.join([x for x in eg if x in cde])
+    g = ''.join([x for x in eg if x != e])
 
     # déduction de d dans cde
-    d = cde.replace(c, '').replace(e, '')
-
+    d = ''.join([x for x in cde if x != c and x != e])
     # déduction de b dans bd
-    b = bd.replace(d, '')
+    b = ''.join([x for x in bd if x != d])
 
     # a, b, c, d, e, f, g
     return {
-        sorted(a + b + c + e + f + g): 0,
+        ''.join(sorted(a + b + c + e + f + g)): 0,
         segments[0]: 1,
-        sorted(a + c + d + e + g): 2,
-        sorted(a + c + d + f + g): 3,
+        ''.join(sorted(a + c + d + e + g)): 2,
+        ''.join(sorted(a + c + d + f + g)): 3,
         segments[2]: 4,
-        sorted(a + b + d + f + g): 5,
-        sorted(a + b + d + e + f + g): 6,
+        ''.join(sorted(a + b + d + f + g)): 5,
+        ''.join(sorted(a + b + d + e + f + g)): 6,
         segments[1]: 7,
         segments[9]: 8,
-        sorted(a + b + c + d + f + g): 9
+        ''.join(sorted(a + b + c + d + f + g)): 9
     }
 
 appearances = [0] * 10
+numbers = []
 for line in inputs:
     wiring = get_wiring(line['segments'])
+    number = 0
     for digit in line['digits']:
         appearances[wiring[digit]] += 1
+        number = number * 10 + wiring[digit]
+
+    numbers.append(number)
+    print(number)
 
 for i in range(0, 10):
     print(f"The digit {i} appears {appearances[i]} times")
 
-print(sum(appearances))
+print(sum(appearances), sum(numbers))
