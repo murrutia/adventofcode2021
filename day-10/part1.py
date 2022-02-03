e486e771764dff4ddc2ac239a55798c3ef305680
Mickael Urrutia <mickael.urrutia@utc.fr>
2022-02-03 17:16:30 +0100

    part1 du jour 10 terminé

--- day-10/part1.py
+++ day-10/part1.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from collections import deque
 
 data_file = Path(__file__).absolute().parent / 'data.txt'
-data_file = Path(__file__).absolute().parent / 'data-example.txt'
+# data_file = Path(__file__).absolute().parent / 'data-example.txt'
 
 couples = {
     ')': '(',
@@ -20,22 +20,27 @@ points = {
     '>': 25137
 }
 
-def check_not_corrupted(line):
+def score_corrupted(line):
     openers = deque()
     for x in line:
         if x in points:
             if len(openers) == 0 or openers[-1] != couples[x]:
-                return False
-    return True
+                print(f"illegal : {x}")
+                return points[x]
+            else:
+                openers.pop()
+        else:
+            openers.append(x)
+    return 0
 
 
 lines = []
+score = 0
 with open(data_file, 'r') as fd:
     line = fd.readline().strip()
     while line:
-        if check_not_corrupted(line):
-            lines.append(line)
-            print(line)
+        s = score_corrupted(line)
+        score += s
         line = fd.readline().strip()
 
-print(lines)
+print(score)
